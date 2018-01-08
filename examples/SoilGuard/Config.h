84b434e4a023ba4a7a90e8096d7099aed05fb9e3
daud <dragondaud@gmail.com>
2018-01-08 15:15:25 -0700

    soilguard

--- examples/SoilGuard/Config.h
+++ examples/SoilGuard/Config.h
@@ -1,6 +1,7 @@
 #include <SolarGuardn.h>
 #include "userconfig.h"
 #ifndef USERCONFIG
+#define HOST "SoilGuard"
 #define WIFI_SSID "SSID"
 #define WIFI_PASS "PASS"
 #define MQTT_SERV "mqtt.local"
@@ -8,11 +9,13 @@
 #define MQTT_TOPIC "SolarGuardn"
 #define MQTT_USER ""
 #define MQTT_PASS ""
-#define BETWEEN 60000           // delay between readings in loop()
 #define gMapsKey "APIKEY"       // https://developers.google.com/maps/documentation/timezone/intro
 #endif
 
-SolarGuardn sg(MQTT_SERV, MQTT_PORT, MQTT_TOPIC, MQTT_USER, MQTT_PASS, gMapsKey);
+WiFiClient    wificlient;
+PubSubClient  mqtt(wificlient);
+
+SolarGuardn sg(HOST, WIFI_SSID, WIFI_PASS, MQTT_SERV, MQTT_PORT, MQTT_TOPIC, MQTT_USER, MQTT_PASS, gMapsKey, &Serial, mqtt);
 
 #define DHTPIN  D4    // DHT22 temp/humid sensor
 #define DHTTYPE DHT22
@@ -23,11 +26,6 @@ SolarGuardn sg(MQTT_SERV, MQTT_PORT, MQTT_TOPIC, MQTT_USER, MQTT_PASS, gMapsKey)
 #define SNUM 3        // number of samples to average
 
 #include <DHT.h>                  // install DHT using library manager
-
-WiFiClient    wifiClient;
-PubSubClient  MQTTclient(wifiClient);
-
-long    TIMER;
 
 DHT dht(DHTPIN, DHTTYPE);
 
--- examples/SoilGuard/SoilGuard.ino
+++ examples/SoilGuard/SoilGuard.ino
@@ -8,134 +8,48 @@
 void setup() {
   Serial.begin(115200);
   //Serial.setDebugOutput(true);
-  while (!Serial);
-  sg.flushIn(Serial);
-  delay(100);
-  Serial.println();
-  Serial.print(F("setup: WiFi connecting to "));
-  Serial.print(WIFI_SSID);
-  Serial.print(F("..."));
-  WiFi.persistent(false);
-  WiFi.mode(WIFI_STA);
-  if (sg.host == "") {
-    String t = WiFi.macAddress();
-    sg.host = String(HOST) + "-" + t.substring(9, 11) + t.substring(12, 14) + t.substring(15, 17);
-  }
-  WiFi.hostname(sg.host);
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(F("."));
-    delay(500);
-  }
-  MDNS.begin(sg.host.c_str());
-  Serial.println(F(" OK"));
-
-  if (sg.location == "") {
-    sg.location = sg.getIPlocation();
-  } else {
-    sg.getIPlocation();
-    sg.location = sg.getLocation(sg.location, gMapsKey);
-  }
-  sg.setNTP();
-
-  ArduinoOTA.onStart([]() {
-    Serial.println(F("\nOTA: Start"));
-  } );
-  ArduinoOTA.onEnd([]() {
-    Serial.println(F("\nOTA: End"));
-    delay(500);
-  } );
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.print("OTA Progress: " + String((progress / (total / 100))) + " \r");
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.print("\nError[" + String(error) + "]: ");
-    if (error == OTA_AUTH_ERROR) Serial.println(F("Auth Failed"));
-    else if (error == OTA_BEGIN_ERROR) Serial.println(F("Begin Failed"));
-    else if (error == OTA_CONNECT_ERROR) Serial.println(F("Connect Failed"));
-    else if (error == OTA_RECEIVE_ERROR) Serial.println(F("Receive Failed"));
-    else if (error == OTA_END_ERROR) Serial.println(F("End Failed"));
-    else Serial.println(F("unknown error"));
-    delay(5000);
-    ESP.restart();
-  });
-  ArduinoOTA.begin();
-
-  MQTTclient.setServer(MQTT_SERV, MQTT_PORT);
-  sg.mqttConnect(MQTTclient);
-
+  sg.setup();
   pinMode(BUILTIN_LED, OUTPUT);
   pinMode(MGND, OUTPUT);          // moisture sensor ground
   digitalWrite(MGND, LOW);
   pinMode(MPOW, OUTPUT);          // moisture sensor power
   digitalWrite(MPOW, LOW);
-
+  delay(100);
   dht.begin();
-  delay(1000);
-  sg.UPTIME = time(nullptr);
-
-  Serial.print(F("Last reset reason: "));
-  Serial.println(ESP.getResetReason());
-  Serial.print(F("WiFi Hostname: "));
-  Serial.println(sg.host);
-  Serial.print(F("WiFi IP addr: "));
-  Serial.println(WiFi.localIP());
-  Serial.print(F("WiFi gw addr: "));
-  Serial.println(WiFi.gatewayIP());
-  Serial.print(F("WiFi MAC addr: "));
-  Serial.println(WiFi.macAddress());
-  Serial.print(F("ESP Sketch size: "));
-  Serial.println(ESP.getSketchSize());
-  Serial.print(F("ESP Flash free: "));
-  Serial.println(ESP.getFreeSketchSpace());
-  Serial.print(F("ESP Flash Size: "));
-  Serial.println(ESP.getFlashChipRealSize());
-  SaveCrash.print(Serial);
+  delay(1000);  // wait for sensors to stabalize
 } // setup
 
 void loop() {
-  if (WiFi.localIP() == IPAddress(0, 0, 0, 0)) {
-    String t = "Invalid null address";
-    sg.mqttPublish(MQTTclient, "debug", t);
-    Serial.println(t);
-    delay(500);
-    ESP.restart();
-  }
-  ArduinoOTA.handle();
-  MQTTclient.loop();
-  if (millis() > TIMER) {
-    TIMER = millis() + BETWEEN;
-  } else {
-    delay(5000);
-    return;
-  }
-  analogWrite(BUILTIN_LED, 1);
+  if (!sg.loop()) return;
+  sg.ledOn(BUILTIN_LED);
   String ip = WiFi.localIP().toString();
-  float temp, humid;
   int heap = ESP.getFreeHeap();
   time_t now = time(nullptr);
-  if (now > sg.TWOAM) {
-    Serial.println();
-    sg.setNTP();
-  }
   String t = ctime(&now);
   t.trim(); // ctime returns extra whitespace
   String u = sg.upTime(now);
-  if (!readDHT(&humid, &temp)) {
-    TIMER = 0;
-    return;
-  }
+
+  float temp, humid;
+  if (!readDHT(&humid, &temp)) return;
+
   int soil = readMoisture(MOIST, MPOW, SNUM, STIM);
+
   Serial.printf("%s: %s, %d°F, %d%%RH, %d moisture, %s uptime, %d heap \r", \
                 ip.c_str(), t.c_str(), round(temp), round(humid), soil, u.c_str(), heap);
-  t = "{\"temp\": " + String(round(temp)) + ", \"humid\": " + String(round(humid)) + ", \"moist\": " + String(soil) + \
-      ", \"timestamp\": " + String(now) + ", \"freeheap\": " + String(heap) + "}";
-  sg.mqttPublish(MQTTclient, "data", t);
-  for (int i = 23; i < 1023; i++) {
-    analogWrite(BUILTIN_LED, i);
-    delay(2);
-  }
-  analogWrite(BUILTIN_LED, 0);
-  digitalWrite(BUILTIN_LED, HIGH);
+
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["app"] = HOST;
+  root["temp"] = round(temp);
+  root["humid"] = round(humid);
+  root["soil"] = soil;
+  root["ip"] = ip;
+  root["time"] = t;
+  root["timestamp"] = now;
+  root["uptime"] = int(now - sg.UPTIME);
+  root["freeheap"] = heap;
+  root.printTo(t = "");
+  sg.mqttPublish("data", t);
+  sg.ledOff(BUILTIN_LED);
 } // loop
 
